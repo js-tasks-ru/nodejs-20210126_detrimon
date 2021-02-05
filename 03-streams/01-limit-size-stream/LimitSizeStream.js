0f1dfb0e3bc9e5e8429944629666e7fae451d8b7
Detrimon <detrimon@gmail.com>
2021-02-05 08:13:26 +0300

    03 - streams Limit Size Stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this._counter = 0;
   }
 
   _transform(chunk, encoding, callback) {
 
+    if (this._counter + chunk.length > this.limit) {
+      let limitError = new LimitExceededError();
+      return callback(limitError);
+    }
+    
+    this._counter += chunk.length;
+    callback(null, chunk)
   }
 }
 
