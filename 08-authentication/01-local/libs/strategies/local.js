532385902fe30e761aabe9617c19e35740dfa8b3
Detrimon <detrimon@gmail.com>
2021-02-23 11:27:14 +0300

    08-authentication 01-local

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,26 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      console.log(email, password);
+      await User.findOne({'email': email}, async function(err, account) {
+        console.log('ACCOUNT??? ', account);
+        if (err) return done(err);
+
+        if (!account) {          
+          return done(err, false, 'Нет такого пользователя');
+        }
+
+        let isPasswordValid = await account.checkPassword(password);
+        if (!isPasswordValid) {
+          return done(err, false, 'Неверный пароль');
+        }
+        
+        done(null, account);
+
+      })
+
     },
 );
