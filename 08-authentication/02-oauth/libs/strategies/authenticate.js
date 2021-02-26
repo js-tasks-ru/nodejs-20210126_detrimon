ac674341c88af26c154b21bee421eabfdf1287d8
Detrimon <detrimon@gmail.com>
2021-02-26 16:50:33 +0300

    08-authentication 02-oauth

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,25 @@
+const User = require('../../models/User');
+const passport = require('passport')
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+
+  if (!email) return done(null, false, 'Не указан email');
+
+  User.findOne({'email': email})
+    .then(oAccount => {
+      if (oAccount) return done(null, oAccount);
+
+      let oDocument = {'email': email, 'displayName': displayName};
+
+      User.create(oDocument)
+          .then(user => done(null, user))
+          .catch(err => done(err))
+    })
+    .catch (err => {
+      console.log(err);
+    })
+
+
+  // Если пользователя нет в базе, то создаем для пользователя новый документ в базе и аутентифицируем после создания.
+  // done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
 };
