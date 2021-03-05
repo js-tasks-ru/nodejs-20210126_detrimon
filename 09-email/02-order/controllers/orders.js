c332841c4e0a78041a1f7c23cf30c8e6a5d07a79
Detrimon <detrimon@gmail.com>
2021-03-05 17:03:07 +0300

    09-email 02-order

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -2,7 +2,35 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  let oOrder = ctx.request.body;
+  oOrder.user = ctx.user;
+  try {
+    let createdOrder = await Order.create(oOrder);
+
+    await sendMail({
+      to: ctx.user.email,
+      subject: 'Подтверждение заказа',
+      locals: {id: createdOrder._id, product: { title: createdOrder.product}},
+      template: 'order-confirmation',
+    });
+
+    ctx.status = 200;
+    ctx.body = { order: createdOrder._id };
+
+  } catch(err) {
+    throw err;
+  }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  let oUser = ctx.user;
+
+  try {
+    let oOrders = await Order.find({ 'user': oUser });
+    ctx.status = 200;
+    ctx.body = { orders: oOrders };
+  } catch(err) {
+    ctx.status = 500;
+    ctx.body = { error: err };
+  }
 };
