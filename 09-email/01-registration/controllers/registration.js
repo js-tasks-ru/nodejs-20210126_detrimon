275964e3a375e3f1278f621e93e7808295809291
Detrimon <detrimon@gmail.com>
2021-03-13 08:13:36 +0300

    09-email Исправление замечаний 01-registration / 02-order

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -6,24 +6,19 @@ module.exports.register = async (ctx, next) => {
   let sToken = uuid();
   let { email, displayName, password } = ctx.request.body;
 
-  try {
     let user = new User({ 'email': email, 'displayName': displayName, 'verificationToken': sToken });
     await user.setPassword(password);
     await user.save();
+    console.log(`<a href="http://localhost:3000/confirm/${sToken}">Подтвердите почту</a>`);
     
     let oMail = { template: 'confirmation',
                   locals: { token: sToken },
                   to: email,
-                  subject: `<a href="http://localhost:3000/confirm/${sToken}">Подтвердите почту</a>` };
+                  subject: "Подтверждение регистрации на сайте!" };
     await sendMail(oMail);
 
     ctx.status = 200;
     ctx.body = {status: 'ok'};
-
-  } catch(err) {
-    ctx.status = 400;
-    ctx.body = {'errors': {'email': err.errors.email.message }};
-  }
 };
 
 module.exports.confirm = async (ctx, next) => {
@@ -31,10 +26,14 @@ module.exports.confirm = async (ctx, next) => {
   if (!verificationToken || verificationToken === 'randomtoken') { // Здесь по идее должна быть проверка на невалидный токен
     ctx.status = 400;
     return ctx.body = { error: 'Ссылка подтверждения недействительна или устарела' };
-
   }
 
-  await User.updateOne({'verificationToken': verificationToken}, { $unset: { 'verificationToken': 1 }} );
+  let updateResult = await User.updateOne({'verificationToken': verificationToken}, { $unset: { 'verificationToken': 1 }} );
+  if (updateResult.n !== 1 && updateResult.Modified !== 1) {
+    ctx.status = 400;
+    return ctx.body = { error: 'Ссылка подтверждения недействительна или устарела' };
+  }
+  
   ctx.status = 200;
   ctx.body = { 'token': verificationToken };
 };
--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -4,33 +4,34 @@ const sendMail = require('../libs/sendMail');
 module.exports.checkout = async function checkout(ctx, next) {
   let oOrder = ctx.request.body;
   oOrder.user = ctx.user;
-  try {
-    let createdOrder = await Order.create(oOrder);
+  let createdOrder = await Order.create(oOrder);
 
-    await sendMail({
-      to: ctx.user.email,
-      subject: 'Подтверждение заказа',
-      locals: {id: createdOrder._id, product: { title: createdOrder.product}},
-      template: 'order-confirmation',
-    });
+  await sendMail({
+    to: ctx.user.email,
+    subject: 'Подтверждение заказа',
+    locals: {
+      id: createdOrder._id,
+      product: {
+        title: createdOrder.product
+      }
+    },
+    template: 'order-confirmation',
+  });
 
-    ctx.status = 200;
-    ctx.body = { order: createdOrder._id };
-
-  } catch(err) {
-    throw err;
-  }
+  ctx.status = 200;
+  ctx.body = {
+    order: createdOrder._id
+  };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
   let oUser = ctx.user;
 
-  try {
-    let oOrders = await Order.find({ 'user': oUser });
-    ctx.status = 200;
-    ctx.body = { orders: oOrders };
-  } catch(err) {
-    ctx.status = 500;
-    ctx.body = { error: err };
-  }
+  let oOrders = await Order.find({
+    'user': oUser
+  });
+  ctx.status = 200;
+  ctx.body = {
+    orders: oOrders
+  };
 };
