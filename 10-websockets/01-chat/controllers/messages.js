11e71e9b4b397e1ea0e8d6bdf4fa96ba7d7182ff
Detrimon <detrimon@gmail.com>
2021-03-13 22:21:04 +0300

    10-websockets 01-chat

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,16 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  let aMessages = await Message.find({ user: ctx.user.displayName });
+  let aRespMessages = [];
+  for (let oMessage of aMessages) {
+    let oRespMessage = {
+      id: oMessage._id,
+      user: oMessage.user,
+      date: oMessage.date,
+      text: oMessage.text
+    }
+    aRespMessages.push(oRespMessage);
+  }
+  ctx.body = {messages: aRespMessages};
 };
